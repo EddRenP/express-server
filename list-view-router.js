c843a43f8f9ecbb9c6d2180cf05ccf73685e3489
Eduardo Rendon Paz <eduardorendon99@gmail.com>
2023-10-03 15:53:00 -0600

    septimo commit, middlewares, revision y arreglo de codigo

--- list-view-router.js
+++ list-view-router.js
@@ -5,9 +5,9 @@ const router = express.Router();
 let tareas = require('./tareas.json');
 
 //middleware valida que el parametro recibido dentro de la ruta de list-view sea el correcto, de lo contrario tira error
-const middValidarurl = ((req, res, next) => {
+const middValidarparametro = ((req, res, next) => {
   const parametro = req.params.parametro;
-   if (parametro == 'mostrar') {
+   if (parametro == 'true' || parametro == 'false') {
      console.log("Url "+ req.originalUrl+ " valida, continuando...");
      next();
    }
@@ -20,42 +20,62 @@ const middValidarurl = ((req, res, next) => {
  });
 
 //metodo que muestra las tareas completadas
-router.get("/completadas/:parametro", middValidarurl, (req, res) => {
+router.get("/:parametro", middValidarparametro, (req, res) => {
+    const parametro = req.params.parametro;
     let completadas = [];
+    let nocompletadas = [];
     let j = 0;
+    let k = 0;
     for (let i = 0; i < tareas.length; i++) {
         if (tareas[i].completado == true) {
             completadas[j] = tareas[i];
             j++;
         }
-    }
-    if (completadas.length>0) {
-        res.status(200).send(completadas);
-      } else {
-        res.status(404).send({
-          mensaje: "no hay tareas completadas",
-        });
-      }
-});
-
-//metodo que muestra las tareas incompletas
-router.get("/nocompletadas/:parametro",middValidarurl, (req, res) => {
-    let nocompletadas = [];
-    let k = 0;
-    for (let i = 0; i < tareas.length; i++) {
-        if (tareas[i].completado == false) {
+        else{
             nocompletadas[k] = tareas[i];
             k++;
         }
     }
-    if (nocompletadas.length>0) {
-        res.status(200).send(nocompletadas);
-      } else {
-        res.status(404).send({
-          mensaje: "no hay tareas incompletas",
-        });
-      }
-    
+    if (parametro == 'true')
+    {
+      if (completadas.length>0) {
+          res.status(200).send(completadas);
+        } else {
+          res.status(404).send({
+            mensaje: "no hay tareas completadas",
+          });
+        }
+    }
+    else
+    {
+      if (nocompletadas.length>0) {
+          res.status(200).send(nocompletadas);
+        } else {
+          res.status(404).send({
+            mensaje: "no hay tareas incompletas",
+          });
+        }
+    }
 });
+
+//metodo que muestra las tareas incompletas
+// router.get("/nocompletadas/:parametro",middValidarurl, (req, res) => {
+//     let nocompletadas = [];
+//     let k = 0;
+//     for (let i = 0; i < tareas.length; i++) {
+//         if (tareas[i].completado == false) {
+//             nocompletadas[k] = tareas[i];
+//             k++;
+//         }
+//     }
+//     if (nocompletadas.length>0) {
+//         res.status(200).send(nocompletadas);
+//       } else {
+//         res.status(404).send({
+//           mensaje: "no hay tareas incompletas",
+//         });
+//       }
+    
+// });
 
 module.exports = router;
